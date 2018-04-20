6debacaa410e053de24da02a2bbb38c0f6af09e6
EE1 <ee1@MM1-IMAC-14.local>
2018-04-20 13:54:01 +0200

    git commit m-

--- js/test.js
+++ js/test.js
@@ -2,4 +2,25 @@ console.log("hola desde un archivo");
 
 var nombrevariable; 
 
-console.log("nombrevariable");
+console.log("nombre de la variable");
+
+var myfavoritephrase = "myfavoritephrase jello";
+console.log(myfavoritephrase.indexOf("j"));
+
+var lenguage = prompt("Bienvenido Usuario: CASTELLANO, INGLES, ITALIANO");
+
+if (lenguage === "CASTELLANO"){
+    console.log("Bienvenido Usuario");
+}else if (lenguage === "ITALIANO"){
+    console.log("Benvenutti Amico");
+}else if(lenguage === "INGLES"){
+    console.log("Welcome User");
+}else{
+    prompt("elige bien capullo");
+}
+
+    
+
+    
+
+
