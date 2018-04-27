69c19dd2901492a2c401f778769d646232a391d0
EE1 <ee1@MM1-IMAC-14.local>
2018-04-27 09:53:57 +0200

    commit

--- js1.js
+++ js1.js
@@ -1,3 +1,6 @@
+var num1 = 8;
+var num2 =10;
+
 function maxNumbers(num1, num2) {
     if (num1 > num2){
         console.log(num1 + " es mayor que " + num2);
@@ -6,8 +9,9 @@ function maxNumbers(num1, num2) {
         console.log(num2 + " es mayor que " + num1);
     }    
 }
+maxNumbers(num1, num2);
 
-// encuentra la palabra mas larga    
+// ejercicio 2 encuentra la palabra mas larga    
 
 var Words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
@@ -35,3 +39,56 @@ function sum(arrayNum) {
     });
 }
 sum(numbers);
+
+// forma num 2
+
+function sumReduce(arrayNum) {
+    var sum = arrayNum.reduce(function (acumulativo, valorAactual) {
+        return acumulativo + valorAactual
+    }, 100);
+    return sum;
+    
+}
+ var arrayShort = [1, 2, 3 ,4];
+
+sumReduce(numbers);
+sumReduce(arrayShort);
+
+function average(arrayNum) {
+    return sumReduce(arrayNum) / arrayNum.length;
+}
+
+console.log(average(numbers));
+
+// ejercicio 5
+
+var words = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+
+function averageWords(arrayWords) {
+    var sumlength = 0;
+    arrayWords.forEach(function(elem) {
+        sum.length += elem.length;
+    });
+    var averageTotal = sum.length / arrayWords.length;
+    return averageTotal;
+}
+
+console.log(averageWords(Words));
+
+// ejercicio 7
+
+var word = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+
+function exists(arrayWord, word) {
+    var exists = false;
+    arrayWord.forEach(function(elem){
+        if (word === elem) {
+            exists = true;
+        }
+    });
+    return exists;
+}
+
+console.log(exists(word));
+
+// ejercicio 8
